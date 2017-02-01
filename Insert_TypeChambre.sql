844266089f8e875b15fc67f5853b9c260d3c2811
Janie <janie.briere@outlook.fr>
2017-02-01 11:37:47 -0500

    insertion

---
+++ Insert_TypeChambre.sql
@@ -0,0 +1,9 @@
+INSERT INTO TypeCham(CodTypCham,DescTyp,NbDispo) VALUES
+('1J','1 LIT JUMEAU','1'),
+('2J','2 LITS JUMEAUX','1'),
+('1D','1 LIT DOUBLE','1'),
+('2D','2 LITS DOUBLE','1'),
+('LQ','LIT QUEEN','1'),
+('LK','LIT KING','1'),
+('ST','SUITE','1'),
+('SR','SALLE RÉCEPTION','1')
