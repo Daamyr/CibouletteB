850b7e0126a8c0a5032c90c5ca2c91a2e91968dc
Daamyr <maxime.damour974@gmail.com>
2017-02-06 15:00:19 -0500

    Ajout projet toutes les parties vides

---
+++ Prj_Systeme/Prj_Chambre_Janie/Prj_Chambre_Janie/Chambre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prj_Chambre_Janie
+{
+    public partial class Chambre : Prj_lib_graphique.Form1
+    {
+        public Chambre()
+        {
+            InitializeComponent();
+        }
+    }
+}
