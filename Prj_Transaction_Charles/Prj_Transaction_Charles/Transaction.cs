7da3c29447f882797cf52b857e7bd4d8f901aac2
Charles Artur Brouard III <charles.brouard@collegeshawinigan.ca>
2017-02-06 10:52:42 -0500

    Transaction
    Ajouté pour la première fois.
    
    
    :)

---
+++ Prj_Transaction_Charles/Prj_Transaction_Charles/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prj_Transaction_Charles
+{
+    public partial class Transaction : Prj_lib_graphique.Form1
+    {
+        public Transaction()
+        {
+            InitializeComponent();
+        }
+    }
+}
