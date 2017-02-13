573f97ae23a6e5ca53d10df0312d82a97e5e5813
james <james@DESKTOP-268JQ7S>
2017-02-12 21:07:54 -0500

    Ceci est un test de commit sur VS

--- Prj_Systeme/Prj_Reservation_James/Prj_Reservation_James/Reservation.Designer.cs
+++ Prj_Systeme/Prj_Reservation_James/Prj_Reservation_James/Reservation.Designer.cs
@@ -28,9 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logo_CB1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(566, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OH PUTAIN";
+            // 
+            // Reservation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(1335, 608);
+            this.Name = "Reservation";
+            ((System.ComponentModel.ISupportInitialize)(this.logo_CB1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
